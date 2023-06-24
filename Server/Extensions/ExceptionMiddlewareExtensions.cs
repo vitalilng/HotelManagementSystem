f0da2f95c4c540581085c504ee58ec79a90d0e92
Vitali <vitalilng@gmail.com>
2023-06-24 15:54:36 +0300

    Update ExceptionMiddlewareExtensions.cs

--- Server/Extensions/ExceptionMiddlewareExtensions.cs
+++ Server/Extensions/ExceptionMiddlewareExtensions.cs
@@ -18,18 +18,28 @@ namespace HotelManagementSystem.Server.Extensions
         /// <param name="loggerManager"></param>
         public static void ConfigureExceptionHandler(this WebApplication app, ILoggerManager loggerManager)
         {
-            ///he UseExceptionHandler middleware is added to the application pipeline. 
+            ///The UseExceptionHandler middleware is added to the application pipeline. 
             ///This middleware is responsible for handling exceptions that occur during the processing of HTTP requests.
             ///It takes a callback function (appError) as an argument.
             app.UseExceptionHandler(appError =>
             {
+                ///Run method is called to handle the exception asynchronously
+                ///context is the current HTTP request context
                 appError.Run(async context =>
                 {
+                    //response content type is set to "application/json"
                     context.Response.ContentType = "application/json";
-
+                    
+                    //IExceptionHandlerFeature is retrieved from the request context features
+                    //IExceptionHandlerFeature contains information about the exception that occured during request processing.                    
                     var contextFeature = context.Features.Get<IExceptionHandlerFeature>();
+                    
+                    //if not null, indicates that an exception occured, then the exception handling logic is executed
                     if (contextFeature != null)
                     {
+                        //this code sets the response status code based on the type of exception
+                        //if the exception is of type NotFoundException, the status code is set to 404
+                        //otherwise is set to 500 InternalServerError
                         context.Response.StatusCode = contextFeature.Error switch
                         {
                             NotFoundException => StatusCodes.Status404NotFound,
@@ -37,6 +47,10 @@ namespace HotelManagementSystem.Server.Extensions
                         };
 
                         loggerManager.LogError($"Something went wrong: {contextFeature.Error}");
+
+                        //This code writes the error details to the response as JSON
+                        //It creates an ErrorDetails object containing the status code and exception message
+                        //ErrorDetails object is then serialized to JSON using the ToString()
                         await context.Response.WriteAsync(new ErrorDetails()
                         {
                             StatusCode = context.Response.StatusCode,
